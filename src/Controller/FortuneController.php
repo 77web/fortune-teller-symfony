1ac9aea1445670a63740d79daf994d85e8b27cae
77web <info@77-web.com>
2021-02-27 00:53:47 +0900

    今日の運勢を表示する画面を作成

--- src/Controller/FortuneController.php
+++ src/Controller/FortuneController.php
@@ -2,12 +2,30 @@
 
 namespace App\Controller;
 
+use App\Domain\ShowFortuneException;
+use App\Domain\ShowFortuneUseCase;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FortuneController extends AbstractController
 {
+    /**
+     * @var ShowFortuneUseCase
+     */
+    private $showFortuneUseCase;
+
+    /**
+     * FortuneController constructor.
+     * @param ShowFortuneUseCase $showFortuneUseCase
+     */
+    public function __construct(ShowFortuneUseCase $showFortuneUseCase)
+    {
+        $this->showFortuneUseCase = $showFortuneUseCase;
+    }
+
     /**
      * @Route("/", name="fortune_index")
      * @return Response
@@ -15,5 +33,26 @@ class FortuneController extends AbstractController
     public function index(): Response
     {
         return $this->render('fortune/index.html.twig');
+    }
+
+    /**
+     * @Route("/show", name="fortune_show")
+     * @param Request $request
+     * @return Response
+     * @throws \Exception
+     */
+    public function show(Request $request): Response
+    {
+        $birthday = new \DateTimeImmutable($request->query->get('birthday'));
+
+        try {
+            $fortune = $this->showFortuneUseCase->showFortune($birthday);
+        } catch (ShowFortuneException $e) {
+            throw new BadRequestHttpException();
+        }
+
+        return $this->render('fortune/show.html.twig', [
+            'fortune' => $fortune,
+        ]);
     }
 }
--- tests/Controller/FortuneControllerTest.php
+++ tests/Controller/FortuneControllerTest.php
@@ -2,10 +2,13 @@
 
 namespace App\Tests\Controller;
 
+use Liip\TestFixturesBundle\Test\FixturesTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class FortuneControllerTest extends WebTestCase
 {
+    use FixturesTrait;
+
     public function test_index()
     {
         $client = static::createClient();
@@ -13,5 +16,31 @@ class FortuneControllerTest extends WebTestCase
 
         $response = $client->getResponse();
         $this->assertTrue($response->isOk(), $response->getStatusCode());
+    }
+
+    public function test_show()
+    {
+        $client = static::createClient();
+        $this->loadFixtureFiles([
+            __DIR__.'/../../fixtures/fortune_controller.yaml',
+        ]);
+
+        $crawler = $client->request('GET', '/show?birthday=1981-01-01');
+
+        $response = $client->getResponse();
+        $this->assertTrue($response->isOk(), $response->getStatusCode());
+
+        $this->assertTrue($crawler->filter('body:contains("今日の運勢は最高です！")')->count() === 1);
+    }
+
+    public function test_show_該当する占いがない()
+    {
+        $client = static::createClient();
+        $this->loadFixtureFiles([]); // データクリアする
+
+        $client->request('GET', '/show?birthday=1981-01-01');
+
+        $response = $client->getResponse();
+        $this->assertEquals(400, $response->getStatusCode());
     }
 }
