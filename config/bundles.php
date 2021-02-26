419525f222148331ebb88c296db2b2d3554281bc
77web <info@77-web.com>
2021-02-27 00:34:05 +0900

    stof/doctrine-extensions-bundle を追加（createdAt, updatedAtを活かすため）

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
